1f2815bfb03e2a45671db30407c14f27f1f70223
Apoorva <40380945+Apoorvashirikanth@users.noreply.github.com>
2023-11-16 13:46:44 +0530

    auth apis

---
+++ Backend/controllers/userControllers.js
@@ -0,0 +1,63 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../config/generateToken");
+
+const registerUser = asyncHandler(async (req, res) => {
+try{  
+   const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).json({message: "Please Enter all the Feilds"});
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400).json({message: "User already exists"});
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  });
+
+  if (user) {
+    console.log("user created successfully")
+    res.status(201).json({
+      message: "User created successfully",
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400).json({message: "Something went wrong"});
+  }
+}catch(err) {
+    console.log("something went wrong")
+  }
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({message: "Please Enter all the Feilds"});
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      message: "User logged in successfully",
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401).json({message:"Invalid Email or Password"});
+  }
+});
+
+module.exports = { registerUser, authUser};
--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes")
 
 dotenv.config();
 connectDB();
 const app = express();
 
 app.use(express.json()); 
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT;
 
