4f8573ebb1c52a645913a08906a1a87ebe0c458d
Apoorva <40380945+Apoorvashirikanth@users.noreply.github.com>
2023-11-16 12:01:52 +0530

    project config

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,13 @@
-console.log("started")
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json()); 
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
