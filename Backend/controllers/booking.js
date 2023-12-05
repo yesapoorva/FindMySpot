3e4d3c29ed44142a500c22c3bfb9007075e0a1e2
Apoorva <40380945+Apoorvashirikanth@users.noreply.github.com>
2023-12-05 13:26:15 +0530

    current bookings

--- Backend/controllers/booking.js
+++ Backend/controllers/booking.js
@@ -36,4 +36,42 @@ const getBookings = async (req, res) => {
   }
 };
 
-  module.exports = { getBookings }
+const getCurrentBookings = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const currentDateTime = new Date();
+
+    const bookings = await Booking.find({
+      user: userId,
+      fromTime: { $lte: currentDateTime },
+      toTime: null
+    });
+
+    const bookingsWithDetails = await Promise.all(
+      bookings.map(async (booking) => {
+        const parkingSpace = await ParkingSpace.findById(booking.parkingSpace);
+
+        if (!parkingSpace) {
+          console.error(`Parking space not found for booking ID: ${booking._id}`);
+          return {
+            parkingSpaceName: 'Unknown',
+            fromTime: booking.fromTime,
+            toTime: booking.toTime,
+          };
+        }
+
+        return {
+          parkingSpaceName: parkingSpace.name,
+          fromTime: booking.fromTime,
+          toTime: booking.toTime,
+        };
+      })
+    );
+
+    res.json(bookingsWithDetails);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+  module.exports = { getBookings , getCurrentBookings}
--- Backend/routes/booking.js
+++ Backend/routes/booking.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {requireSignin} = require('../middleware/requireSignin'); 
-const { getBookings } = require('../controllers/booking');
+const { getBookings , getCurrentBookings} = require('../controllers/booking');
 
 router.get('/bookings', requireSignin , getBookings);
+router.get('/currentBookings', requireSignin , getCurrentBookings);
 
 module.exports = router;
