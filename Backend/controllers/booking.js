76484406a2dc4a68c7589cafee2adda991ab054b
Apoorva <40380945+Apoorvashirikanth@users.noreply.github.com>
2023-12-05 10:27:17 +0530

    bookigs modified

--- Backend/controllers/booking.js
+++ Backend/controllers/booking.js
@@ -2,28 +2,38 @@ const ParkingSpace = require("../models/parkingSpace");
 const Booking = require('../models/booking'); 
 
 const getBookings = async (req, res) => {
-    try {
-      const userId = req.user.id; 
-  
-      const bookings = await Booking.find({ user: userId });
-  
-      const bookingsWithDetails = await Promise.all(
-        bookings.map(async (booking) => {
-          const parkingSpace = await ParkingSpace.findById(booking.parkingSpace);
+  try {
+    const userId = req.user.id;
+
+    const bookings = await Booking.find({ user: userId });
+
+    const bookingsWithDetails = await Promise.all(
+      bookings.map(async (booking) => {
+        
+        const parkingSpace = await ParkingSpace.findById(booking.parkingSpace);
+
+        if (!parkingSpace) {
+          console.error(`Parking space not found for booking ID: ${booking._id}`);
           return {
-            parkingSpaceName: parkingSpace.name,
-          //   date: booking.date,
+            parkingSpaceName: 'Unknown',
             fromTime: booking.fromTime,
             toTime: booking.toTime,
           };
-        })
-      );
-  
-      res.json(bookingsWithDetails);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+        }
+
+        return {
+          parkingSpaceName: parkingSpace.name,
+          fromTime: booking.fromTime,
+          toTime: booking.toTime,
+        };
+      })
+    );
+
+    res.json(bookingsWithDetails);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
+};
 
   module.exports = { getBookings }
